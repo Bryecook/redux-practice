43a9aa54a993105f12d2d7969d3a63b7b9ecb3c5
Bryecook <brybcook@gmail.com>
2021-06-23 10:30:30 -0500

    reducer/redux initial set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 import Posts from './components/Posts';
-import Postform from './components/Postform'
+import Postform from './components/Postform';
+import { applyMiddleware } from 'redux';
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -24,6 +28,7 @@ function App() {
       <hr />
       <Posts />
     </div>
+    </Provider>
   );
 }
 
